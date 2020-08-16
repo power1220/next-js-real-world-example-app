43bc7a16d9fa6dbe81f0a7383b0f765a8e7884aa
power1220 <power012200@gmail.com>
2020-08-16 10:06:03 +0300

    update register page

--- pages/user/register.tsx
+++ pages/user/register.tsx
@@ -6,26 +6,26 @@ import RegisterForm from "../../components/profile/RegisterForm";
 
 const Register = () => (
   <>
-  <Head>
-    <title>REGISTER | NEXT REALWORLD</title>
-    <meta name="description" content="Please register before login" />
-  </Head>
-  <div className="auth-page">
-    <div className="container page">
-      <div className="row">
-        <div className="col-md-6 offset-md-3 col-xs-12">
-          <h1 className="text-xs-center">Sign Up</h1>
-          <p className="text-xs-center">
-            <CustomLink href="/user/login" as="/user/login">
-              Have an account?
-            </CustomLink>
-          </p>
+    <Head>
+      <title>REGISTER | NEXT REALWORLD</title>
+      <meta name="description" content="Please register before login" />
+    </Head>
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign Up</h1>
+            <p className="text-xs-center">
+              <CustomLink href="/user/login" as="/user/login">
+                Have an account?
+              </CustomLink>
+            </p>
 
-          <RegisterForm />
+            <RegisterForm />
+          </div>
         </div>
       </div>
     </div>
-  </div>
   </>
 );
 
